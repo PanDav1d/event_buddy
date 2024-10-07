c02fd7ec87c64d66f4bd5a72e94fa14a4b70112c
PanDav1d <muellerdavid.3008@gmail.com>
2024-10-07 22:30:37 +0200

    new api in c# and some new app ui features

--- event/constants/DateQuickTabs.ts
+++
@@ -1,8 +0,0 @@
-export enum DateQuickTab
-{
-    today = "Heute",
-    tomorrow = "Morgen",
-    this_weekend = "Dieses Wochenende",
-    next_week = "Nächste Woche",
-    this_month = "Diesen Monat",
-}
