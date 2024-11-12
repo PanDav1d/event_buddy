78a4b62d7a77373c45ad70d725196fb7db5ecc79
PanDav1d <muellerdavid.3008@gmail.com>
2024-11-12 19:58:42 +0100

    feat:migrations for database

---
+++ eventbuddy_api/Migrations/20241112183920_AddTicketUrl.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eventbuddy_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTicketUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TicketUrl",
+                table: "Event",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TicketUrl",
+                table: "Event");
+        }
+    }
+}
