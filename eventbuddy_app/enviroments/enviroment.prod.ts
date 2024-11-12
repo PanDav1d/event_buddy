a79f5f4a729a8f2488631c60f10ef3157516be37
PanDav1d <muellerdavid.3008@gmail.com>
2024-11-12 18:06:16 +0100

    impl:enviroments folder[C

---
+++ eventbuddy_app/enviroments/enviroment.prod.ts
@@ -0,0 +1,3 @@
+export abstract class Enviroment {
+  public static BASE_URL: string = 'http://localhost:5196/api/v1';
+}
---
+++ eventbuddy_app/enviroments/enviroment.ts
@@ -0,0 +1,3 @@
+export abstract class Enviroment {
+  public static BASE_URL: string = 'https://eventbuddy.bsite.net/api/v1';
+}
